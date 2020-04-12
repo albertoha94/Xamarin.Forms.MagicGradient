1ff2b3e6c4c8b80a1750191d16108ae75f247f03
Albertoha94-L <albertoha94@hotmail.com>
2020-04-12 13:23:17 -0500

    Initial commit

---
+++ Xamarin.MagicGradient/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Xamarin.MagicGradient.ViewModels
+{
+    public class ItemsViewModel : BaseViewModel
+    {
+        public Command LoadItemsCommand { get; set; }
+
+        public ItemsViewModel()
+        {
+            Title = "Gradients Example";
+        }
+    }
+}
---
+++ Xamarin.MagicGradient/Views/ItemsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+using Xamarin.MagicGradient.ViewModels;
+
+namespace Xamarin.MagicGradient.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
